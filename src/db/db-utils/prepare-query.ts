d1daab568c8e95c75e6dfb0778fb64e518e914fd
ahmd1c <gwerqweldiv@gmail.com>
2024-08-12 13:51:02 +0300

    improve query preparing functions

--- src/db/db-utils/prepare-query.ts
+++ src/db/db-utils/prepare-query.ts
@@ -1,8 +1,8 @@
 import { PgTableWithColumns } from "drizzle-orm/pg-core";
-import { TProjection, TSearchedEntry } from "../../types";
+import { TProjection } from "../../types";
 import { and, eq, getTableColumns } from "drizzle-orm";
 
-export const projectionFunction = <T extends PgTableWithColumns<any>>(
+export const project = <T extends PgTableWithColumns<any>>(
   fields: TProjection<T["$inferSelect"]> | undefined,
   table: T
 ) => {
@@ -10,14 +10,14 @@ export const projectionFunction = <T extends PgTableWithColumns<any>>(
   const selectedFields: T["_"]["columns"] = {};
   for (const field of Object.keys(fields)) {
     if (fields[field]) {
-      selectedFields[field] = table._.columns[field];
+      selectedFields[field] = getTableColumns(table)[field];
     }
   }
   return selectedFields;
 };
 
-export const filterationFunction = <T extends PgTableWithColumns<any>>(
-  filterObj: TSearchedEntry<T["$inferSelect"]>,
+export const filter = <T extends PgTableWithColumns<any>>(
+  filterObj: Partial<T["$inferSelect"]>,
   table: T
 ) => {
   const filterArr = Object.entries(filterObj).map(([key, value]) => {
