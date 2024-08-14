d728f201536ca9c72e96db3ecde1da81408f9487
ahmd1c <gwerqweldiv@gmail.com>
2024-08-14 20:03:29 +0300

    added get profile handler

--- src/users/usersController.ts
+++ src/users/usersController.ts
@@ -15,6 +15,16 @@ export const insertProfileController = asyncHandler(async (req, res, next) => {
   res.status(201).json({ profile });
 });
 
+export const getProfile = asyncHandler(async (req, res, next) => {
+  const profile = await UserServicesInstance.getProfile(+req.params.id);
+  res.status(200).json({ profile });
+});
+
+export const getSelfProfile = asyncHandler(async (req, res, next) => {
+  const profile = await UserServicesInstance.getProfile(req.user?.id!);
+  res.status(200).json({ profile });
+});
+
 export const updateProfileController = asyncHandler(async (req, res, next) => {
   const profile = await UserServicesInstance.updateProfile(req.user?.id!, req.body.bio);
   res.status(201).json({ profile });
